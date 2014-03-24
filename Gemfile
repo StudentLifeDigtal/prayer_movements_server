60f780a20fbbb0135778aa95452435f464124d9d
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-03-24 16:27:00 +1300

    Initial Server Commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem 'rails-api'
+gem 'grape'
+gem 'grape-swagger'
+gem 'puma'
+gem 'pg'
---
+++ app/controllers/api/v1/movements.rb
@@ -0,0 +1,8 @@
+module API
+  module V1
+    class Movements < Grape::API
+      include API::V1::Defaults
+      
+    end
+  end
+end
