ec7ed2d6c901c5a0a71a5f0fa074da8d8018af34
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-04-03 20:59:32 +1300

    added testing for oath

--- spec/requests/api/oauth_spec.rb
+++ spec/requests/api/oauth_spec.rb
@@ -1,2 +1,21 @@
 # spec/requests/api/oauth_spec.rb
 require 'spec_helper'
+
+describe 'OAuth authorization' do
+  let(:app) { FactoryGirl.create :application }
+  let(:user) { FactoryGirl.create :user }
+
+  let(:client) { OAuth2::Client.new(app.uid, app.secret) do |b|
+    b.request :url_encoded
+    b.adapter :rack, Rails.application
+  end }
+
+  it 'auth ok' do
+    token = client.password.get_token(user.email, user.password)
+    expect(token).to_not be_expired
+  end
+
+  it 'auth not ok' do
+    expect {client.password.get_token(user.email, "123")}.to raise_error(OAuth2::Error)
+  end
+end
