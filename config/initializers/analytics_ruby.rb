3039a344e2933f2ad2a1656cef69c7a558b00489
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-04-04 00:36:38 +1300

    let travis execute even if analytics isn't setup correctly

--- config/initializers/analytics_ruby.rb
+++ config/initializers/analytics_ruby.rb
@@ -1,5 +1,8 @@
 Analytics = AnalyticsRuby       # Alias for convenience
-Analytics.init({
-    secret: ENV['analytics_key'],          # The write key for slnz/prymv
-    on_error: Proc.new { |status, msg| print msg }  # Optional error handler
-})
+
+unless ENV['analytics_key'].nil?
+	Analytics.init({
+	    secret: ENV['analytics_key'],          # The write key for slnz/prymv
+	    on_error: Proc.new { |status, msg| print msg }  # Optional error handler
+	})
+end
